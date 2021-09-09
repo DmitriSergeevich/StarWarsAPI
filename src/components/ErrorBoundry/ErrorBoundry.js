e7d72b9c396afe956f4312cb240878eef0d8c92c
Dmitry Likhachev <valomon@yandex.ru>
2021-09-09 16:59:27 +0500

    feat: add build

--- src/components/ErrorBoundry/ErrorBoundry.js
+++ src/components/ErrorBoundry/ErrorBoundry.js
@@ -4,14 +4,11 @@ import { ErrorIndicator } from "../ErrorIndicator/ErrorIndicator";
 export class ErrorBoundry extends React.Component {
   state = {
     hasError: false
-  }
-  
-  
+  }  
 
   componentDidCatch() {
     this.setState({hasError: true})    
-  }
-  
+  }  
 
   render() {
     if(this.state.hasError) {
